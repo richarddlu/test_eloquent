460d1b33dab4fd4e648f1599ac771bc046cb8f79
Lu <sunlufinal@hotmail.com>
2015-08-01 02:52:43 -0400

    Tests of Eloquent ORM.

--- database/migrations/2015_07_28_020305_test_migration.php
+++ database/migrations/2015_07_28_020305_test_migration.php
@@ -26,6 +26,7 @@ class TestMigration extends Migration
             $table->integer('user_id')->unsigned()->nullable();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
+            $table->softDeletes();
         });
 
         Schema::create('phones', function(Blueprint $table)
@@ -45,6 +46,20 @@ class TestMigration extends Migration
             $table->string('photoable_type');
             $table->timestamps();
         });
+
+        Schema::create('default_test', function(Blueprint $table)
+        {
+            $table->increments('id');
+            $table->string('string_not_null_no_default');
+            $table->string('string_not_null_default')->default('default string');
+            $table->string('string_null_no_default')->nullable();
+            $table->string('string_null_default')->nullable()->default('default string');
+            $table->integer('integer_not_null_no_default');
+            $table->integer('integer_not_null_default')->default(5);
+            $table->integer('integer_null_no_default')->nullable();
+            $table->integer('integer_null_default')->nullable()->default(5);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -54,6 +69,7 @@ class TestMigration extends Migration
      */
     public function down()
     {
+        Schema::drop('default_test');
         Schema::drop('photos');
         Schema::drop('phones');
         Schema::drop('posts');
--- tests/EloquentTest.php
+++ tests/EloquentTest.php
@@ -6,6 +6,7 @@ use App\User;
 use App\Post;
 use App\Phone;
 use App\Photo;
+use App\DefaultTest;
 
 class EloquentTest extends TestCase
 {
@@ -75,13 +76,13 @@ class EloquentTest extends TestCase
         // timestamp -> Carbon
         $this->assertInstanceOf('Carbon\Carbon', $user->created_at);
 
-        // NULL -> null
+        // NULL -> NULL
         $post = Post::find(4);
         $this->assertInternalType('null', $post->content);
 
-        // empty string
+        // empty varchar -> string
         $post = Post::find(5);
-        $this->assertInternalType('string', $post->content);
+        $this->assertSame('', $post->content);
     }
 
     public function testCreateTypeOK()
@@ -104,7 +105,7 @@ class EloquentTest extends TestCase
         $post = Post::find($id);
         $this->assertSame('123', $post->content);
 
-        // null -> NULL
+        // NULL -> NULL
         $post = new Post;
         $post->content = null;
         $post->user_id = 1;
@@ -113,7 +114,7 @@ class EloquentTest extends TestCase
         $post = Post::find($id);
         $this->assertInternalType('null', $post->content);
 
-        // string null -> string
+        // string NULL -> string
         $post = new Post;
         $post->content = 'NULL';
         $post->user_id = 1;
@@ -126,8 +127,135 @@ class EloquentTest extends TestCase
     /**
      * @expectedException Illuminate\Database\QueryException
      */
-    public function testCreateTypeNotOK()
+    public function testCreateTypeStringNullToIntNull()
     {
-        
+        // string NULL -> int NULL
+        $post = new Post;
+        $post->content = '123';
+        $post->user_id = 'NULL';
+        $post->save();
+    }
+
+    public function testCreate()
+    {
+        // test firstOrCreate
+        $post = Post::firstOrCreate(['content' => 'post 1']);   // record exists
+        $this->assertEquals(1, $post->id);
+        $post = Post::firstOrCreate(['content' => 'post xxx']); // record does not exist
+        $post = Post::where('content', '=', 'post xxx')->first();
+        $this->assertEquals('post xxx', $post->content);
+
+        // test firstOrNew
+        $post = Post::firstOrNew(['content' => 'post 1']);   // record exists
+        $this->assertEquals(1, $post->id);
+        $post = Post::firstOrNew(['content' => 'post yyy']); // record does not exist
+        $this->assertEquals('post yyy', $post->content);
+        $this->assertFalse(property_exists($post, 'user_id'));
+        $post = Post::where('content', '=', 'post yyy')->first();
+        $this->assertNull($post);
+    }
+
+    public function testDelete()
+    {
+        // test delete()
+        $phone = Phone::find(1);
+        $this->assertNotNull($phone);
+        $phone = Phone::find(1);
+        $phone->delete();
+        $phone = Phone::find(1);
+        $this->assertNull($phone);
+
+        // test delete() with query builder
+        // Post is soft delete model
+        $posts = Post::where('id', '<', 3)->get();
+        $this->assertFalse($posts->isEmpty());
+        Post::where('id', '<', 3)->delete();
+        $posts = Post::where('id', '<', 3)->get();
+        $this->assertTrue($posts->isEmpty());
+
+        // test destroy()
+        $phone = Phone::find(2);
+        $this->assertNotNull($phone);
+        Phone::destroy(2);
+        $phone = Phone::find(2);
+        $this->assertNull($phone);
+        $photos = Photo::all();
+        $this->assertFalse($photos->isEmpty());
+        Photo::destroy([1,2,3]);
+        $photos = Photo::all();
+        $this->assertTrue($photos->isEmpty());
+
+        // test destopy with non-existing primary key
+        // expect no exception
+        Photo::destroy([1,2,3]);
+        $photos = Photo::all();
+        $this->assertTrue($photos->isEmpty());
+    }
+
+    public function testSoftDelete()
+    {
+        // test trashed()
+        $post = Post::find(1);
+        $post->delete();
+        $this->assertTrue($post->trashed());
+
+        // test withTrashed()
+        Post::find(2)->delete();
+        $post = Post::find(2);
+        $this->assertNull($post);
+        $post = Post::withTrashed()->find(2);
+        $this->assertNotNull($post);
+
+        // test onlyTrashed()
+        $post = Post::find(3);
+        $this->assertNotNull($post);
+        $post = Post::onlyTrashed()->find(3);
+        $this->assertNull($post);
+
+        // test restore
+        $post = Post::find(3);
+        $post->delete();
+        $post->restore();
+        $post1 = Post::find(3);
+        $this->assertNull($post1);
+        $post = Post::onlyTrashed()->find(3);
+        $post->restore();
+        $post1 = Post::find(3);
+        $this->assertNotNull($post1);
+    }
+
+    public function testScope()
+    {
+        // test static scope
+        $posts = Post::findID3()->get();
+        $this->assertEquals(1, $posts->count());
+
+        // test dynamic scope
+        $posts = Post::findIDAndContent(5, '')->get();
+        $this->assertEquals(1, $posts->count());
+    }
+
+    public function testDefaultValue()
+    {
+        $test = new DefaultTest;
+        $test->save();
+        $id = $test->id;
+        $test = DefaultTest::find($id);
+        // string_not_null_no_default
+        $this->assertSame('', $test->string_not_null_no_default);
+        // string_not_null_default
+        $this->assertEquals('default string', $test->string_not_null_default);
+        // string_null_no_default
+        $this->assertNull($test->string_null_no_default);
+        // string_null_default
+        $this->assertEquals('default string', $test->string_null_default);
+        // integer_not_null_no_default
+        $this->assertSame(0, $test->integer_not_null_no_default);
+        // integer_not_null_default
+        $this->assertEquals(5, $test->integer_not_null_default);
+        // integer_null_no_default
+        $this->assertNull($test->integer_null_no_default);
+        // integer_null_default
+        $this->assertEquals(5, $test->integer_null_default);
     }
 }
